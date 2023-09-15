bf473b8fdf817c5a64cebcd61d2f5a1edee915ac
mmisty <minydas@yandex.ru>
2023-09-15 17:33:33 +0300

    fix descrtructure

---
+++ tests/test-folder/mocha-events/config/no-video.test.ts
@@ -0,0 +1,45 @@
+import { checkCyResults, createResTest2 } from '../../../cy-helper/utils';
+import { AllureTest, parseAllure } from 'allure-js-parser';
+
+describe('test video event', () => {
+  const res = createResTest2(
+    [
+      `
+describe('test video false', () => {
+
+  it('test fails', () => {
+    cy.wait(3000);
+    cy.wrap(null).then(()=> {
+       throw new Error('on purpose')
+    })
+  });
+  
+  it('test pass', () => {});
+});
+`,
+    ],
+    { video: 'false' },
+  );
+
+  describe('check results', () => {
+    let results: AllureTest[];
+
+    beforeAll(() => {
+      results = parseAllure(res.watch);
+    });
+
+    it('check cypress results', () => {
+      checkCyResults(res?.result?.res, { totalPassed: 1, totalFailed: 1 });
+    });
+
+    it('check test with fail exists', async () => {
+      const test = results.find(t => t.name === 'test fails');
+      expect(test).toBeDefined();
+    });
+
+    it('check test with pass exists', async () => {
+      const test = results.find(t => t.name === 'test pass');
+      expect(test).toBeDefined();
+    });
+  });
+});
