f67b15123bee974d5580dbfdb0eff9f52e7730a4
Taisia Pitko <minydas@yandex.ru>
2023-11-01 12:22:31 +0300

    [patch] fix of #59 issue and update cypress (#60)

---
+++ integration/e2e/hooks/before-each-fail-in-test-then-in-hook.cy.ts
@@ -0,0 +1,24 @@
+describe('before each fail with retry - first in test then in hook @beforeEachRetry', { retries: 2 }, () => {
+  beforeEach('Named hook', function () {
+    cy.log('before each');
+
+    if (Cypress.currentRetry > 1 && this.currentTest?.title?.indexOf('test 05') !== -1) {
+      // throw new Error('Fail in hook');
+      cy.wrap(null).then(() => {
+        throw new Error('Fail in hook');
+      });
+    }
+  });
+
+  for (let i = 1; i <= 10; i++) {
+    it(`test ${`0${i}`.slice(-2)}`, function () {
+      cy.log(`test ${i}`);
+
+      if (this.test?.title.indexOf('test 05') !== -1) {
+        cy.wrap(null).then(() => {
+          throw new Error('Fail in test');
+        });
+      }
+    });
+  }
+});
