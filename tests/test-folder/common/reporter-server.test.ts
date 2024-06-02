d4f059b505e4cb47a0957e763cc46c635a84a63a
mmisty <minydas@yandex.ru>
2024-06-02 16:34:34 +0200

    fix server test for plugins

--- tests/test-folder/common/reporter-server.test.ts
+++ tests/test-folder/common/reporter-server.test.ts
@@ -1,13 +1,20 @@
 import WebSocket, { WebSocketServer } from 'ws';
-import { consoleMock } from '../../mocks/console-mock';
 import { existsSync, rmSync } from 'fs';
+import { AllureTest, parseAllure } from 'allure-js-parser';
+import { consoleMock } from '../../mocks/console-mock';
 
 jest.setTimeout(60000);
 const results = 'reports/allure-res';
-// need to fix process hanging in tests
-// eslint-disable-next-line jest/no-disabled-tests
-describe.skip('startReporterServer', () => {
-  const start = async (
+
+describe('startReporterServer', () => {
+  beforeEach(() => {
+    consoleMock();
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    const messages = require('../../../src/plugins/server').testMessages;
+    messages.splice(0, messages.length);
+  });
+
+  const start = (
     debug: boolean,
     env: any,
   ): Promise<{ serv: undefined | WebSocketServer }> => {
@@ -33,240 +40,210 @@ describe.skip('startReporterServer', () => {
     serv.serv = startReporterServer({ env } as any, reporter);
 
     // wait to start
-    await new Promise(res => {
-      setInterval(() => {
+    return new Promise((res, rej) => {
+      setTimeout(() => {
         if (env['allureWsPort'] !== undefined) {
           res(null);
+        } else {
+          rej('Ws not started');
         }
-      });
+      }, 1000);
+    }).then(() => {
+      return serv;
     });
-
-    return serv;
   };
 
-  it('should start ws server', async () => {
-    const log = consoleMock();
+  const startWsAndClient = (
+    sendObjs: any[],
+    callback: (back: string[]) => void,
+  ) => {
     const env = {};
-    const serv = await start(true, env);
 
-    const wsPathFixed =
-      `${env['allureWsPort']}/__cypress/allure_messages/`.replace(/\/\//g, '/');
-    const wsPath = `ws://localhost:${wsPathFixed}`;
-    const ws = new WebSocket(wsPath, { origin: 'localhost' });
-    const messages: string[] = [];
+    const back: any[] = [];
 
-    const isOpened = await new Promise(res => {
-      ws.on('error', err => {
-        console.error(err);
-        res(false);
-      });
-      ws.on('open', function open() {
-        ws.send('OPENED');
-      });
-      ws.on('message', function msg(m) {
-        messages.push(m.toString());
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    const messages = require('../../../src/plugins/server').testMessages;
+    messages.splice(0, messages.length);
 
-        if (m.toString() === 'connection established') {
-          res(true);
-        }
-      });
-    }).catch(err => {
-      console.log(err);
+    console.log(messages);
 
-      return false;
-    });
-    ws.close();
-
-    await new Promise(res => {
-      serv.serv!.close(() => {
-        res(true);
-      });
-    });
-
-    expect(isOpened).toEqual(true);
-
-    try {
-      expect(log.log.mock.calls.length).toEqual(1);
-    } catch (err) {
-      const mess = log.log.mock.calls;
-      console.info(mess);
-      throw err;
+    if (existsSync(results)) {
+      rmSync(results, { recursive: true });
     }
-    expect(log.log.mock.calls[0].map(t => t.replace(/\d+/g, 'number'))).toEqual(
-      ['[cypress-allure-adapter] running on number port'],
+
+    return start(true, env).then(serv => {
+      const wsPathFixed =
+        `${env['allureWsPort']}/__cypress/allure_messages/`.replace(
+          /\/\//g,
+          '/',
+        );
+      const wsPath = `ws://localhost:${wsPathFixed}`;
+      const ws = new WebSocket(wsPath, { origin: 'localhost' });
+
+      return new Promise(res => {
+        ws.on('error', err => {
+          console.error(err);
+          res(false);
+        });
+        ws.on('open', function open() {
+          ws.send('OPENED');
+        });
+        ws.on('message', function msg(m) {
+          if (`${m}` !== 'connection established') {
+            back.push(`${m}`);
+          }
+          sendObjs.forEach((obj, i) => {
+            ws.send(
+              JSON.stringify({
+                id: i + 1,
+                ...obj,
+              }),
+            );
+          });
+
+          res(true);
+        });
+      })
+        .catch(err => {
+          console.log(err);
+
+          return false;
+        })
+        .then(() => {
+          return new Promise((resolveAllGot, rejectGotAll) => {
+            const started = Date.now();
+
+            const interval = setInterval(() => {
+              if (messages.length < sendObjs.length + 1) {
+                // +1 - connection established message
+                if (Date.now() - started > 4000) {
+                  rejectGotAll('Not received all');
+                }
+              } else {
+                clearInterval(interval);
+                resolveAllGot(true);
+              }
+            });
+          })
+            .then(() => {
+              let closed = false;
+              ws.close();
+              serv.serv!.on('close', () => {
+                closed = true;
+              });
+              serv.serv!.close();
+
+              return new Promise((res, rej) => {
+                setTimeout(() => {
+                  if (closed) {
+                    console.log('SERVER CLOSED');
+                    res(null);
+                  } else {
+                    rej('Ws server was not closed');
+                  }
+                }, 1000);
+              });
+            })
+            .then(() => {
+              callback(back);
+            });
+        });
+    });
+  };
+
+  it('should do some tasks', () => {
+    return startWsAndClient(
+      [
+        {
+          data: { task: 'specStarted', arg: { spec: { relative: 'speccc' } } },
+        },
+        {
+          data: {
+            task: 'testStarted',
+            arg: { title: 'test1', fullTitle: 'test1', id: '1' },
+          },
+        },
+        { data: { task: 'testEnded', arg: { result: 'passed' } } },
+      ],
+      back => {
+        expect(back).toEqual([
+          '{"status":"done"}',
+          '{"payload":{"task":"specStarted","arg":{"spec":{"relative":"speccc"}}},"status":"done"}',
+          '{"payload":{"task":"testStarted","arg":{"title":"test1","fullTitle":"test1","id":"1"}},"status":"done"}',
+          '{"payload":{"task":"testEnded","arg":{"result":"passed"}},"status":"done"}',
+        ]);
+        expect(existsSync(results)).toEqual(true);
+      },
     );
-    expect(messages).toEqual(['connection established']);
   });
 
-  it('should do some tasks', async () => {
-    const env = {};
-
-    if (existsSync(results)) {
-      rmSync(results, { recursive: true });
-    }
-    await start(true, env);
-
-    const wsPathFixed =
-      `${env['allureWsPort']}/__cypress/allure_messages/`.replace(/\/\//g, '/');
-    const wsPath = `ws://localhost:${wsPathFixed}`;
-    const ws = new WebSocket(wsPath, { origin: 'localhost' });
-    const messages: string[] = [];
-
-    const isSent = await new Promise(res => {
-      ws.on('error', err => {
-        console.error(err);
-        res(false);
-      });
-      ws.on('open', function open() {
-        ws.send('OPENED');
-      });
-      ws.on('message', function msg(m) {
-        messages.push(m.toString());
-
-        if (m.toString() === 'connection established') {
-          ws.send(
-            JSON.stringify({
-              id: 1,
-              data: {
-                task: 'specStarted',
-                arg: { spec: { relative: 'speccc' } },
-              },
-            }),
-          );
-          ws.send(
-            JSON.stringify({
-              id: 2,
-              data: {
-                task: 'testStarted',
-                arg: { title: 'test1', fullTitle: 'test1', id: '1' },
-              },
-            }),
-          );
-          ws.send(
-            JSON.stringify({
-              id: 3,
-              data: { task: 'testEnded', arg: { result: 'passed' } },
-            }),
-          );
-        }
-
-        if (messages.length === 3) {
-          res(true);
-        }
-      });
-    }).catch(err => {
-      console.log(err);
-
-      return false;
-    });
-    ws.close();
-    expect(isSent).toEqual(true);
-
-    expect(messages).toEqual([
-      'connection established',
-      '{"status":"done"}',
-      '{"payload":{"task":"specStarted","arg":{"spec":{"relative":"speccc"}}},"status":"done"}',
-      '{"payload":{"task":"testStarted","arg":{"title":"test1","fullTitle":"test1","id":"1"}},"status":"done"}',
-      '{"payload":{"task":"testEnded","arg":{"result":"passed"}},"status":"done"}',
-    ]);
-    expect(existsSync(results)).toEqual(true);
+  it('should not do unknown task', () => {
+    return startWsAndClient(
+      [
+        {
+          data: { task: 'blslsl', arg: {} },
+        },
+      ],
+      back => {
+        expect(back).toEqual([
+          '{"status":"done"}',
+          '{"payload":{"task":"blslsl","arg":{}},"status":"failed"}',
+        ]);
+        expect(existsSync(results)).toEqual(true);
+      },
+    );
   });
 
-  it('should not do unknown task', async () => {
-    const env = {};
+  it('should not do when no task', () => {
+    return startWsAndClient([{ data: {}, id: 1 }], back => {
+      expect(back).toEqual([
+        '{"status":"done"}',
+        '{"payload":{},"status":"failed"}',
+      ]);
 
-    if (existsSync(results)) {
-      rmSync(results, { recursive: true });
-    }
-    await start(true, env);
-
-    const wsPathFixed =
-      `${env['allureWsPort']}/__cypress/allure_messages/`.replace(/\/\//g, '/');
-    const wsPath = `ws://localhost:${wsPathFixed}`;
-    const ws = new WebSocket(wsPath, { origin: 'localhost' });
-    const messages: string[] = [];
-
-    const isSent = await new Promise(res => {
-      ws.on('error', err => {
-        console.error(err);
-        res(false);
-      });
-      ws.on('open', function open() {
-        ws.send('OPENED');
-      });
-      ws.on('message', function msg(m) {
-        messages.push(m.toString());
-
-        if (m.toString() === 'connection established') {
-          ws.send(JSON.stringify({ id: 1, data: { task: 'blslsl', arg: {} } }));
-        }
-
-        if (messages.length === 2) {
-          res(true);
-        }
-      });
-    }).catch(err => {
-      console.log(err);
-
-      return false;
+      expect(existsSync(results)).toEqual(true);
     });
-    ws.close();
-    expect(isSent).toEqual(true);
-
-    expect(messages).toEqual([
-      'connection established',
-      '{"status":"done"}',
-      '{"payload":{"task":"blslsl","arg":{}},"status":"failed"}',
-    ]);
-    expect(existsSync(results)).toEqual(true);
   });
 
-  it('should not execute  when not task', async () => {
-    const env = {};
+  it('should write result', () => {
+    return startWsAndClient(
+      [
+        {
+          data: { task: 'specStarted', arg: { spec: { relative: 'speccc' } } },
+        },
+        {
+          data: {
+            task: 'testStarted',
+            arg: { title: 'test1', fullTitle: 'test1', id: '1' },
+          },
+        },
+        { data: { task: 'testEnded', arg: { result: 'passed' } } },
 
-    if (existsSync(results)) {
-      rmSync(results, { recursive: true });
-    }
-    await start(true, env);
+        {
+          data: {
+            task: 'testResult',
+            arg: { title: 'test1', id: 'testID1', result: 'passed' },
+          },
+        },
+      ],
+      () => {
+        expect(existsSync(results)).toEqual(true);
 
-    const wsPathFixed =
-      `${env['allureWsPort']}/__cypress/allure_messages/`.replace(/\/\//g, '/');
-    const wsPath = `ws://localhost:${wsPathFixed}`;
-    const ws = new WebSocket(wsPath, { origin: 'localhost' });
-    const messages: string[] = [];
-
-    const isSent = await new Promise(res => {
-      ws.on('error', err => {
-        console.error(err);
-        res(false);
-      });
-      ws.on('open', function open() {
-        ws.send('OPENED');
-      });
-      ws.on('message', function msg(m) {
-        messages.push(m.toString());
-
-        if (m.toString() === 'connection established') {
-          ws.send(JSON.stringify({ id: 1, data: {} }));
-        }
-
-        if (messages.length === 2) {
-          res(true);
-        }
-      });
-    }).catch(err => {
-      console.log(err);
-
-      return false;
-    });
-    ws.close();
-    expect(isSent).toEqual(true);
-
-    expect(messages).toEqual([
-      'connection established',
-      '{"status":"done"}',
-      '{"payload":{},"status":"failed"}',
-    ]);
-    expect(existsSync(results)).toEqual(true);
+        const pa: AllureTest[] = parseAllure(`${results}/watch`);
+        expect(
+          pa.map(t => ({
+            name: t.name,
+            fullName: t.fullName,
+            status: t.status,
+          })),
+        ).toEqual([
+          {
+            fullName: 'test1',
+            name: 'test1',
+            status: 'passed',
+          },
+        ]);
+      },
+    );
   });
 });
