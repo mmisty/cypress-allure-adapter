6dd67b8ceb0df9a8b8d4154814c30f45f3d8b9e6
mmisty <minydas@yandex.ru>
2024-06-29 00:40:19 +0200

    fix problem

--- tests/test-folder/mocha-events/hooks/suites/suites.test.ts
+++ tests/test-folder/mocha-events/hooks/suites/suites.test.ts
@@ -2,7 +2,7 @@ import { createResTest2, generateChecksTests, TestData } from '@test-utils';
 
 describe('suites diff tests', () => {
   // test
-  const testsForOneCyRun: TestData[] = ['before-and-after-failure.test.ts'].map(
+  const testsForOneCyRun: TestData[] = ['before-and-after-failure.ts'].map(
     x => require(`${__dirname}/${x}`).default,
   );
 
