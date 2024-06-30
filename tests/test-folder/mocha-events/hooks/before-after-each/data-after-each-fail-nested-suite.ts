7197efcd6aebc15f2c0959a13f83539ec513c7d9
Taisia Pitko <minydas@yandex.ru>
2024-07-01 01:14:18 +0200

    Update data-after-each-fail-nested-suite.ts

--- tests/test-folder/mocha-events/hooks/before-after-each/data-after-each-fail-nested-suite.ts
+++ tests/test-folder/mocha-events/hooks/before-after-each/data-after-each-fail-nested-suite.ts
@@ -105,7 +105,7 @@ describe('${rootSuite}', () => {
               {
                 attachments: [],
                 name: 'wrap',
-                status: 'passed',
+                status: 'failed',
                 steps: [],
               },
             ],
