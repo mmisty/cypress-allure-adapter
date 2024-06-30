bd8e898ba480bc749a96ce79e756692b7d23454b
Taisia Pitko <minydas@yandex.ru>
2024-07-01 01:14:45 +0200

    Update data-after-each-fail-several-tests.ts

--- tests/test-folder/mocha-events/hooks/before-after-each/data-after-each-fail-several-tests.ts
+++ tests/test-folder/mocha-events/hooks/before-after-each/data-after-each-fail-several-tests.ts
@@ -104,7 +104,7 @@ describe('${rootSuite}', () => {
               {
                 attachments: [],
                 name: 'wrap',
-                status: 'passed',
+                status: 'failed',
                 steps: [],
               },
             ],
