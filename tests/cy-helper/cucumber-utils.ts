d69e074cd290c2287f8ffd65c621dfa26f7e4e67
mmisty <minydas@yandex.ru>
2024-07-01 16:31:08 +0200

    fix build

--- tests/cy-helper/cucumber-utils.ts
+++ tests/cy-helper/cucumber-utils.ts
@@ -1,6 +1,7 @@
 import { existsSync, mkdirSync, writeFileSync } from 'fs';
 import { basename } from 'path';
 import { parseBoolean } from 'cypress-redirect-browser-log/utils/functions';
+import expect from 'expect';
 
 type Res = {
   res:
