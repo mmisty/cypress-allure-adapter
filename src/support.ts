0c4b5ffd7b89855a071e6575d910e21b4dd93a07
Taisia Pitko <taisia.pitko@greenlight.guru>
2024-07-05 12:28:23 +0200

    fix types

--- src/support.ts
+++ src/support.ts
@@ -1,5 +1,3 @@
-// eslint-disable-next-line @typescript-eslint/triple-slash-reference
-/// <reference path="./cypress/cypress.ts" />
 import { allureAdapterSetup } from './setup';
 
 allureAdapterSetup();
