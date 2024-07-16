b0c521379a0c8d920031c67639396a955ca05ecf
Taisia Pitko <taisia.pitko@greenlight.guru>
2024-07-16 13:58:17 +0200

    fix type

--- src/commands/session.ts
+++ src/commands/session.ts
@@ -1,21 +1,21 @@
 export const wrapSessionCmd = () => {
   const sess = cy.session;
 
-  (global as unknown as { cy: Cypress.cy }).cy.session = (id: string, ...args: unknown[]) => {
+  (global as unknown as { cy: Cypress.cy }).cy.session = (id, setup, options, ...args: unknown[]) => {
     if (`${Cypress.env('allureLogCyCommands')}` === 'true' || Cypress.env('allureLogCyCommands') === undefined) {
       cy.doSyncCommand(() => {
-        Cypress.Allure.startStep(`session: ${id}`);
+        Cypress.Allure.startStep(`session${typeof id === 'string' ? `: ${id}` : ''}`);
       });
 
       // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-expect-error
-      return sess(id, ...args).should(() => {
+      // @ts-ignore
+      return sess(id, setup, options, ...args).should(() => {
         Cypress.Allure.endStep();
       });
     } else {
       // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-expect-error
-      return sess(id, ...args);
+      // @ts-ignore
+      return sess(id, setup, options, ...args);
     }
   };
 };
