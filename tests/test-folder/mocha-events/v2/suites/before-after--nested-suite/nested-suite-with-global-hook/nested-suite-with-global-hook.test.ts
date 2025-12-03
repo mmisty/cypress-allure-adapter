f01ce378a9cc06cbf2fc06b5308d91f9bcd104a6
Taisia Pitko <taisia.pitko@greenlight.guru>
2025-12-03 13:00:50 +0100

    tests

--- tests/test-folder/mocha-events/v2/suites/before-after--nested-suite/nested-suite-with-global-hook/nested-suite-with-global-hook.test.ts
+++ tests/test-folder/mocha-events/v2/suites/before-after--nested-suite/nested-suite-with-global-hook/nested-suite-with-global-hook.test.ts
@@ -16,7 +16,7 @@ describe('nested suites with failed hook', () => {
   beforeAll(async () => {
     results = await prepareResults(__dirname, {
       env: { allureAddVideoOnPass: 'true' },
-      onlyGetResults: true,
+      // onlyGetResults: true,
     });
   });
   let test1: AllureTest | undefined;
